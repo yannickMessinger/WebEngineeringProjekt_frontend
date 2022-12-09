3a44d6955c8077d7e06ad79080760ea2d834000d
flbohnxx <florian.bohn@student.hs-rm.de>
2022-12-09 15:01:28 +0100

    Added API Key

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,14 +1,14 @@
-import { useState } from "react"
+    import { useState } from "react"
 
 export const useWeather= () => {
     const [location, setLocation] = useState("");
-
+    const API_KEY = "79a9ad1cd477e4c6265d4b1882c856b0";
 
 
     function fetchWeather() {
 
     }
-    
+
     return {
         location,
         setLocation,
