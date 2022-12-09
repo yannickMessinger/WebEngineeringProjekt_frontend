3eac28c316885b701c465a5b49450a3597545771
Lena Ngo <thuy.ngo@student.hs-rm.de>
2022-12-09 15:02:30 +0100

    corrected linked button

--- src/components/Menu/RouterButton.tsx
+++ src/components/Menu/RouterButton.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import { Link, redirect } from "react-router-dom";
 
 interface RouterButtonProps {
-    display: string,
-    to: string
+  display: string;
+  to: string;
 }
 
-export const RouterButton = ({display, to}: RouterButtonProps) => {
-
-    return (
-        <button>
-            <Link to={to}>
-                {display}
-            </Link>
-        </button>
-    )
-}
+export const RouterButton = ({ display, to }: RouterButtonProps) => {
+  return (
+    <Link to={to}>
+      <button>{display}</button>
+    </Link>
+  );
+};
