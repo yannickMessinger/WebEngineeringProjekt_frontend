c3a6d235cdd426d0c5fb02dd0650fb5c76ef9a56
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-12-09 11:52:21 +0100

    init of signup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Question } from './components/Question/questionComponent';
-import { IAnswer } from './components/Answer/IAnswer';
 
-const answers:IAnswer[] = [
-  {
-    text:"Darth Vader", 
-    isRight: true
-  },{
-    text:"Hello Kitty",
-    isRight: false
-  }, {
-    text:"Daisy",
-    isRight: false
-  }]
 
 function App() {
   return (
     <div className="App">
-      <Question 
-        question="Wer ist der Vater von Luke Skywalker?"
-        answerOptions={answers} />
+      <h3>SignUp</h3>
     </div>
   );
 }
