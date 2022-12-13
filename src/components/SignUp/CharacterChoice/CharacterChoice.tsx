edf19fc494ee211e4ba9680ebb6c13b0b7b6b52c
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-12-13 14:39:14 +0100

    layout + styling for login

--- src/components/SignUp/CharacterChoice/CharacterChoice.tsx
+++ src/components/SignUp/CharacterChoice/CharacterChoice.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from './Choice.module.css';
 import logo from '../../../assets/pngwing.com.png'
 import yoda from '../../../assets/yoda.png';
+import boba from '../../../assets/boba.png'
+import darthvader from '../../../assets/darth_vader.png'
+
+
 
 
 export const CharacterChoice = () => {
+
+  const [charPicPath, setCharPicPath] = useState(yoda);
+  const [toggleChar, setToggleChar] = useState(true);
+
+  //TODO: anpasse auf passende prev und next bilder!
+  const charChoiceHandler = () => {
+    
+    if(charPicPath === yoda){
+      setCharPicPath(boba);
+    }else if(charPicPath === boba){
+      setCharPicPath(darthvader);
+    }else if(charPicPath === darthvader){
+      setCharPicPath(yoda);
+    }
+  }
+
+  const handleTogglePicture = () => {
+    setToggleChar(!toggleChar);
+    
+    if(toggleChar){
+      console.log("grünen Rahmen anzeigen")
+    }
+  }
+
+  
+
+  
+
   return (
     
     <div className={css.characterChoice}>
@@ -25,11 +57,11 @@ export const CharacterChoice = () => {
       </div>
 
       <div className={css.choice}>
-        <div className={css.arrowleft}></div>
-          <div className={css.characterDisplay}>
-            <img src={yoda} className={css.character_img}/>
+        <div className={css.arrowleft} onClick = {() => charChoiceHandler()}></div>
+          <div className={`${css.characterDisplay} ${toggleChar ? css.characterDisplayToggle : css.characterDisplay}`} onClick = {() => handleTogglePicture()}>
+            <img src={charPicPath} className={css.character_img}/>
           </div>
-        <div className={css.arrowright}></div>
+        <div className={css.arrowright}  onClick = {() => charChoiceHandler()}></div>
 
       </div>
 
