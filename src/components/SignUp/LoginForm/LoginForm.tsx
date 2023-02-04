54d45161482a233678f3c0a8e7ae24ce09d631f8
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2023-02-04 13:37:42 +0100

    init validation of login form

--- src/components/SignUp/LoginForm/LoginForm.tsx
+++ src/components/SignUp/LoginForm/LoginForm.tsx
@@ -1,8 +1,10 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 import css from "./Loginform.module.css";
 import vader from '../../../assets/darth_choke.png';
 import { CharacterContext } from "../../../context/CharacterContext";
 import { Link } from "react-router-dom";
+import { StyledFormInput } from "./StyledFormInput";
+import { FormError } from "./FormError";
 
 
 export const LoginForm = () => {
@@ -12,46 +14,95 @@ export const LoginForm = () => {
   // ggf doch lieber auf Form ändern?
   const {returnCharacter} = useContext(CharacterContext)
 
-  const firstname = useRef("");
-  const lastname = useRef("");
-  const birthday = useRef()
+  const firstName = useRef("");
+  const lastName = useRef("");
+  const birthday = useRef("")
   const adress = useRef("");
-  const phonenumber = useRef("")
+  const phoneNumber = useRef("")
+  const [formErrors, setFormErrors] = useState({firstName:"", lastName:"",phoneNumber:"", birthday:"", adress:""});
+
+  
+
+
+
+  const validateFormData = (event:any) => {
+    //TODO Regex Expression nutzen für Validierung????
+    event.preventDefault();
+    const errors = {firstName:"", lastName:"",phoneNumber:"", birthday:"",adress:""};
+    
+    if(firstName.current === ''){
+      errors.firstName = "kaa leere vorname";
+    }
+
+    if(lastName.current === ''){
+      errors.lastName = "kaa leere nachname";
+    }
+
+    if(phoneNumber.current === ''){
+      errors.phoneNumber = "kaa leere nummä";
+    }
+
+    if(birthday.current === ''){
+      errors.birthday = "kaa leere geboortsdaach";
+    }
+
+    if(adress.current === ''){
+      errors.adress ="kaa leere Adress"
+    }
+
+
+
+
+    setFormErrors(errors);
+
+    
+
+  }
+
+
   return (
 
     <div className={css.loginGeneral}>
       <div className={css.loginheader}> <img src={returnCharacter().img_path} className={css.loginheaderimg}/>
       </div>
       
+     
       <div className={css.loginform}>
+        
         <div className={css.logininputfields}>
+
           <div className={css.firstname}>
-            <input type="text" placeholder="firstname" className={css.styledinput} value={firstname.current}></input>
+          <StyledFormInput  value={firstName.current} type ={'text'} classname={css.styledinput} placeholder={'firstname'}></StyledFormInput>
+          {formErrors.firstName && <FormError name = {formErrors.firstName}></FormError>}  
           </div>
 
           <div className={css.lastname}>
-            <input type="text" placeholder="lastname"  className={css.styledinput} value={lastname.current}></input>
+          <StyledFormInput  value={lastName.current} type ={'text'} classname={css.styledinput} placeholder={'lastname'}></StyledFormInput>
+          {formErrors.lastName && <FormError name = {formErrors.lastName}></FormError>} 
           </div>
 
           <div className={css.birthday}>
-            <input type="date" placeholder="birthday"  className={css.styledinput} value={birthday.current}></input>
+          <StyledFormInput  value={birthday.current} type ={'date'} classname={css.styledinput} placeholder={'birthday'}></StyledFormInput>
+          {formErrors.birthday && <FormError name = {formErrors.birthday}></FormError>} 
           </div>
 
           <div className={css.adress}>
-            <input type="text" placeholder="adress"  className={css.styledinput} value={adress.current}></input>
+          <StyledFormInput  value={adress.current} type ={'text'} classname={css.styledinput} placeholder={'adress'}></StyledFormInput>
+          {formErrors.adress && <FormError name = {formErrors.adress}></FormError>} 
           </div>
 
           <div className={css.phonenumber}>
-            <input type="text" placeholder="phonenumber"  className={css.styledinput}  value={phonenumber.current}></input>
+          <StyledFormInput  value={phoneNumber.current} type ={'text'} classname={css.styledinput} placeholder={'phonenumber'}></StyledFormInput>
+          {formErrors.phoneNumber && <FormError name = {formErrors.phoneNumber}></FormError>} 
           </div>
         </div>
-
+        
         <div className={css.buttonbox}>
-        <Link to={`/charinfo`}><button>GO</button></Link>
+        <button  onClick={(e) => {validateFormData(e)}}>GO</button>
         </div>
-
+    
       </div>
-
+     
       <div className={css.loginfooter}>FOOTER</div>
       
     </div>
