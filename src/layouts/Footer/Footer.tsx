ae938d7e9f86a799b0e86003d790aeba3edbe275
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2023-02-13 15:37:36 +0100

    hover and cursor styling for clickable objects

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,1 +1,9 @@
-import React from "react";
+
+
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
---
+++ src/pages/InfoOverview/InfoOverview.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const InfoOverview = () => {
+  return (
+    <div>InfoOverview</div>
+  )
+}
