fe909a800a27d4e006f3af533e46c16613a0d6e9
Lena Ngo <thuy.ngo@student.hs-rm.de>
2023-02-15 14:54:55 +0100

    added QuestionType FILL IN THE BLANK

--- src/components/Quiz/Answer/AnswerCard/AnswerCard.tsx
+++ src/components/Quiz/Answer/AnswerCard/AnswerCard.tsx
@@ -4,41 +4,48 @@ import { QuestionType } from "../../Question/IQuestion";
 import { MultipleChoice } from "../MultipleChoice/MultipleChoice";
 import { Estimation } from "../Estimation/Estimation";
 import { Picture } from "../Picture/Picture";
+import { FillInTheBlank } from "../FillInTheBlank/FillInTheBlank";
 
 interface IProps {
-    answerList: IAnswer[],
-    answerImages: {data:any},
-    questionType: QuestionType
-    onClickNext: (wasCorrect:boolean, onCorrect:number, onFalse:number) => void
+    questionText: string;
+    answerList: IAnswer[];
+    answerImages: { data: any };
+    questionType: QuestionType;
+    onClickNext: (
+        wasCorrect: boolean,
+        onCorrect: number,
+        onFalse: number
+    ) => void;
 }
 
-export const AnswerCard = ({answerList, answerImages, questionType, onClickNext}:IProps) => {
-    console.log(questionType)
-    switch(questionType) {
+export const AnswerCard = ({
+    questionText,
+    answerList,
+    answerImages,
+    questionType,
+    onClickNext,
+}: IProps) => {
+    console.log(questionType);
+    switch (questionType) {
         case QuestionType.MULTIPLE_CHOICE:
             return (
                 <MultipleChoice
                     answerList={answerList}
-                    onClickNext={onClickNext} 
+                    onClickNext={onClickNext}
                 />
-            )
+            );
         case QuestionType.ESTIMATION:
             return (
-                <Estimation
-                    answerList={answerList}
-                    onClickNext={onClickNext}
-                />
-            )
+                <Estimation answerList={answerList} onClickNext={onClickNext} />
+            );
         case QuestionType.FILL_IN_THE_BLANK:
             return (
-                <h3>Fill in the blank</h3>
-                /*
                 <FillInTheBlank
+                    questionText={questionText}
                     answerList={answerList}
                     onClickNext={onClickNext}
                 />
-                */
-            )
+            );
         case QuestionType.PICTURE:
             return (
                 <Picture
@@ -46,10 +53,8 @@ export const AnswerCard = ({answerList, answerImages, questionType, onClickNext}
                     answerImages={answerImages}
                     onClickNext={onClickNext}
                 />
-            )
+            );
         default:
-            return (
-                <h3>Fehler bei QuestionType</h3>
-            )
+            return <h3>Fehler bei QuestionType</h3>;
     }
-}
+};
