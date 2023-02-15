3cb39a9e1be751670a940caaafb78e9426526880
Lena Ngo <thuy.ngo@student.hs-rm.de>
2023-02-15 14:46:25 +0100

    updated styling of quiz result

--- src/components/Quiz/QuizResult/QuizResult.tsx
+++ src/components/Quiz/QuizResult/QuizResult.tsx
@@ -2,19 +2,26 @@ import React from "react";
 import css from "./QuizResult.module.css";
 
 interface IProps {
-  finalScore: number;
-  correctAnswers: number
+    finalScore: number;
+    correctAnswers: number;
+    questionAmount: number;
 }
-export const QuizResult = ({ finalScore, correctAnswers }: IProps) => {
-  return (
-    <>
-      <h1 className={css.title}>Geschafft!</h1>
-      <h3 className={css.score}>Du hast {finalScore} Punkte erreicht!</h3>
-      <h3 className={css.score}>{correctAnswers} Fragen richtig beantwortet</h3>
-      <div className={css.Icon}>
-        <div className={css.front}>🔥</div>
-        <div className={css.back}>🌟</div>
-      </div>
-    </>
-  );
+export const QuizResult = ({
+    finalScore,
+    correctAnswers,
+    questionAmount,
+}: IProps) => {
+    return (
+        <div className={css.center}>
+            <h1 className={css.title}>Geschafft!</h1>
+            <h3 className={css.score}>Du hast {finalScore} Punkte erreicht!</h3>
+            <h3 className={css.score}>
+                {correctAnswers} von {questionAmount} Fragen richtig beantwortet
+            </h3>
+            <div className={css.Icon}>
+                <div className={css.front}>🔥</div>
+                <div className={css.back}>🌟</div>
+            </div>
+        </div>
+    );
 };
