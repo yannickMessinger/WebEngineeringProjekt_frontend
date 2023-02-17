b855cdca30b05130780b03e32384fcf295bafdb5
Lena Ngo <thuy.ngo@student.hs-rm.de>
2023-02-17 09:11:00 +0100

    Header is only showed if user is logged in

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import logo from "../../assets/assets_characterChoice/star_wars_logo.png";
 import css from "./Header.module.css";
 import { useNavigate } from "react-router";
+import { LoginContext } from "../../context/LoginContext";
 
 export const Header = () => {
     const navigate = useNavigate();
+    const { isLoggedIn } = useContext(LoginContext);
+    if (!isLoggedIn) {
+        return <></>;
+    }
     return (
-        <div className={css.style} onClick={() => navigate("/")}>
-            <img src={logo} className={css.logo} />
+        <div className={css.style}>
+            <img
+                onClick={() => navigate("/")}
+                src={logo}
+                className={css.logo}
+            />
         </div>
     );
 };
